f916c34c8a6abb0715950a4a88033cdc698aa67d
Petukhov Aleksandr <paryashijvnebe@gmail.com>
2025-03-05 21:39:37 +0300

    fix(handlers) исправил ошибку keyerror
    исправил ошибку + начал выносить логику из хендлеров в UserLogic

--- app/handlers/user.py
+++ app/handlers/user.py
@@ -5,14 +5,14 @@ from aiogram import F, Router
 from aiogram.enums import ChatAction
 from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
+from aiogram.methods import EditMessageText
 from aiogram.types import CallbackQuery, Message, ReplyKeyboardRemove
 
 import app.keyboards as kb
-from app.database.models import Drink
-from app.database.requests.keyboards import get_names
-from app.database.requests.user import UserDataHint, get_drink, get_igredient_photo, set_user
+from app.database.requests.user import UserDataHint
 from app.handlers.admin import ADMIN_IDS
 from app.helpers import delete_messages
+from app.logic.user_logic import UserLogic
 
 user_router = Router()
 
@@ -24,12 +24,13 @@ async def wait_typing(message: Message) -> None:
         await asyncio.sleep(random.uniform(0.1, 0.5))
 
 @user_router.message(CommandStart())
-async def cmd_start(message: Message, state: FSMContext) -> None:
+async def cmd_start(message: Message, state: FSMContext, user_logic: UserLogic) -> None:
     """роутер команды /start.
 
     Args:
         message: объект сообщения.
         state: Состояния памяти.
+        user_logic: логика работы с клиентом.
     """
     if await state.get_data():
         await state.clear()
@@ -40,7 +41,7 @@ async def cmd_start(message: Message, state: FSMContext) -> None:
                                "full_name": message.from_user.full_name,
                                "last_name": message.from_user.last_name,
                                "been_deleted": False}
-    await set_user(user_data)
+    await user_logic.set_user(user_data)
 
     is_admin_user = message.from_user.id in ADMIN_IDS  # Проверяем, является ли пользователь админом
     main_keyboard = kb.create_main_keyboard(is_admin_user)  # Создаем клавиатуру динамически
@@ -58,21 +59,16 @@ async def help_cmd(message: Message) -> None:
     await message.answer("Перейти в главное меню введите /start", reply_markup=ReplyKeyboardRemove())
 
 @user_router.callback_query(F.data == "drinks")
-async def drinks(callback: CallbackQuery, state: FSMContext) -> None:
+async def drinks(callback: CallbackQuery, state: FSMContext, user_logic: UserLogic) -> EditMessageText | None:
     """Роутер колбека кнопки Напитки.
 
     Args:
         callback: объект входящий запрос колбека кнопки обратного вызова на inline keyboard
         state: Состояния памяти.
+        user_logic: логика работы с клиентом.
     """
     await callback.answer("Вы выбрали напитки.")
-    state_data = await state.get_data()
-    if message_ids_to_delete := state_data.get("item_msgs_to_delete"):
-        await delete_messages(callback, message_ids_to_delete)
-        await state.clear()
-    else:
-        names = await get_names(model=Drink)
-        await callback.message.edit_text("Выберете напиток", reply_markup=kb.inline_builder(names))
+    return await user_logic.get_all_drinks(callback, state)
 
 @user_router.callback_query(F.data == "ingredients")
 async def ingredients(callback: CallbackQuery, state: FSMContext) -> None:
@@ -87,61 +83,53 @@ async def ingredients(callback: CallbackQuery, state: FSMContext) -> None:
     if message_ids_to_delete := state_data.pop("ingredient_item_msgs_to_delete", None):
         await state.update_data(ingredient_item_msgs_to_delete=None)
         await delete_messages(callback, message_ids_to_delete)
-        if not state_data.get("item_msgs_to_delete"):
+        await callback.bot.edit_message_reply_markup(chat_id=callback.message.chat.id,
+                                                     message_id=state_data["drink_msgs"]["desc_msg_id"],
+                                                     reply_markup=kb.back_to_drinks)
+        if not state_data.get("drink_msgs"):
             await state.clear()
     else:
-        names = state_data["ingredients"]
-        await callback.message.edit_text("Выберете ингредиент",
-                                         reply_markup=kb.inline_builder(names, item="ingredient_item_"))
+        names = state_data.get("ingredients") or []
+        await callback.message.edit_reply_markup(reply_markup=kb.inline_builder(names, item="ingredient_item_"))
+
 
 @user_router.callback_query(F.data.startswith("drink_item_"))
-async def drink_item_handler(callback: CallbackQuery, state: FSMContext) -> None:
+async def drink_item_handler(callback: CallbackQuery, state: FSMContext, user_logic: UserLogic) -> None:
     """Роутер рассккрывающий карточку напитка.
 
     Args:
         callback: объект входящий запрос колбека кнопки обратного вызова на inline keyboard
         state: Состояния памяти.
+        user_logic: логика работы с клиентом.
     """
     await callback.answer("Вы выбрали напиток")
-    state_data = await state.get_data()
-    if message_ids_to_delete := state_data.get("item_msgs_to_delete"):
-        await delete_messages(callback, message_ids_to_delete)
-        await state.clear()
-    result = await get_drink(item_id=callback.data)
-    await state.update_data(ingredients=result["ingredients"])
-    photo_message = await callback.message.answer_photo(
-        photo=result["photos"][0]["photo_string"],
-        caption=f"Напиток: {result["name"]}",
-        show_caption_above_media=True,
-        )
-    description_message = await callback.message.answer(text=f"Описание напитка: {result["description"]}",
-                                                       reply_parameters={"message_id": photo_message.message_id},
-                                                       reply_markup=kb.back_to_drinks)
-    await state.update_data(item_msgs_to_delete=[photo_message.message_id, description_message.message_id])
+    await user_logic.get_drink_info(callback, state)
 
 @user_router.callback_query(F.data.startswith("ingredient_item_"))
-async def ingredient_item_handler(callback: CallbackQuery, state: FSMContext) -> None:
+async def ingredient_item_handler(callback: CallbackQuery, state: FSMContext, user_logic: UserLogic) -> None:
     """Роутер расскрывающий карточку ингредиента.
 
     Args:
         callback: объект входящий запрос колбека кнопки обратного вызова на inline keyboard
         state: Состояния памяти.
+        user_logic: логика работы с клиентом.
     """
     await callback.answer("Вы выбрали ингредиент")
+    await callback.message.edit_reply_markup()
     state_data = await state.get_data()
     if message_ids_to_delete := state_data.pop("ingredient_item_msgs_to_delete", None):
         await delete_messages(callback, message_ids_to_delete)
         await state.update_data(ingredient_item_msgs_to_delete=None)
-        if not state_data.get("item_msgs_to_delete"):
+        if not state_data.get("drink_msgs"):
             await state.clear()
-    ingredient = await get_igredient_photo(item_id=callback.data)
+    ingredient = await user_logic.get_igredient_photo(item_id=callback.data)
     photo_message = await callback.message.answer_photo(
         photo=ingredient["photos"][0]["photo_string"],
         caption=f"Ингредиент: {ingredient["name"]}",
         show_caption_above_media=True,
         )
     description_message = await callback.message.answer(text=f"Описание ингредиента: {ingredient["description"]}",
-                                                        reply_parameters={"message_id": photo_message.message_id},
+                                                        # reply_parameters={"message_id": photo_message.message_id},
                                                        reply_markup=kb.back_to_ingredients)
     await state.update_data(ingredient_item_msgs_to_delete=[photo_message.message_id, description_message.message_id])
 
@@ -167,6 +155,11 @@ async def back_to_start(callback: CallbackQuery, state: FSMContext) -> None:
         state: состояние памяти.
     """
     await callback.answer("В начало.")
+    state_date = await state.get_data()
+    if msgs_to_del := state_date.get("drink_msgs"):
+        await delete_messages(callback, list(msgs_to_del.values()))
+    if ingredients_to_del := state_date.get("ingredient_item_msgs_to_delete"):
+        await delete_messages(callback, ingredients_to_del)
     await state.clear()
     is_admin_user = callback.from_user.id in ADMIN_IDS
     start_keyboard = kb.create_main_keyboard(is_admin_user)
