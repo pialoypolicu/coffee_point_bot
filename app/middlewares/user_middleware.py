e9a0101252884252f466caa5c2f44b11b12ad783
Petukhov Aleksandr <paryashijvnebe@gmail.com>
2025-03-05 21:25:43 +0300

    feat(middlewares) UserLogickMiddleware + подключение зависимостей
    написана новая зависимость UserLogickMiddleware для роутера user_routree.
    
    подключение middlewares теперь переахали в app/middlewares/base.py

---
+++ app/middlewares/user_middleware.py
@@ -0,0 +1,27 @@
+from collections.abc import Awaitable
+from typing import Any, Callable
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+
+from app.logic.user_logic import UserLogic
+
+
+class UserLogickMiddleware(BaseMiddleware):
+    """Middleware для внедрения LogicFeedback."""
+
+    def __init__(self, user_logic: UserLogic) -> None:
+        """конструктор middleware.
+
+        Args:
+            logic_user: логика работы с клиентом.
+        """
+        self.user_logic = user_logic
+
+    async def __call__(self,
+                       handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+                       event: TelegramObject,
+                       data: dict[str, Any]) -> Any:
+        """Вызоваа middleware."""
+        data["user_logic"] = self.user_logic
+        return await handler(event, data)
