a7dd909ebc03353085a6690fff6263061230073a
Petukhov Aleksandr <paryashijvnebe@gmail.com>
2025-03-16 15:43:39 +0300

    refactor(middlewares) подключаем в base AIGenLogicMiddleware

--- app/middlewares/base.py
+++ app/middlewares/base.py
@@ -1,13 +1,22 @@
+import os
 from typing import Any, Protocol
 
 from aiogram import Dispatcher, Router
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
 
 from app.logger import Logger
+from app.logic.ai_gen_logic import AIGeneratorLogic
 from app.logic.feedback import LogicFeedback
 from app.logic.user_logic import UserLogic
+from app.middlewares.ai_gen_middleware import AIGenLogicMiddleware
 from app.middlewares.feedback import LogicFeedbackMiddleware
 from app.middlewares.logger_middleware import LoggingMiddleware
 from app.middlewares.user_middleware import UserLogickMiddleware
+
+load_dotenv()
+
+ai_connection = AsyncOpenAI(api_key=os.getenv("GPT_TOKEN"), base_url="https://openrouter.ai/api/v1")
 
 
 class RoutersModule(Protocol):
@@ -21,6 +30,7 @@ class RoutersModule(Protocol):
 logger = Logger()
 logic_feedback = LogicFeedback()
 user_logic = UserLogic()
+ai_generator_logic = AIGeneratorLogic(ai_connection)
 
 
 def activate_middlewares(dp: Dispatcher, routers: Any) -> None:
@@ -35,6 +45,8 @@ def activate_middlewares(dp: Dispatcher, routers: Any) -> None:
 
     routers.user_router.message.middleware(UserLogickMiddleware(user_logic))
     routers.user_router.callback_query.middleware(UserLogickMiddleware(user_logic))
+
+    routers.ai_router.callback_query.middleware(AIGenLogicMiddleware(ai_generator_logic))
 
     dp.callback_query.middleware(LoggingMiddleware(logger))
     dp.message.middleware(LoggingMiddleware(logger))
